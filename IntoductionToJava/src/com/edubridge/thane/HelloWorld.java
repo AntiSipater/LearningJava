9f6fb2f8fa814ce2ee2c00e88652ed91448a1a0d
lenovo <lenovo@lenovo-PC-8>
2024-02-26 18:14:56 +0530

    Second Commit

--- IntoductionToJava/src/com/edubridge/thane/HelloWorld.java
+++ IntoductionToJava/src/com/edubridge/thane/HelloWorld.java
@@ -1,11 +1,35 @@
 package com.edubridge.thane;
 
+import java.util.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		String name="Pranay";
+		
+		System.out.println(name);
+		
+		char ch[]= {'M','A','N','S','I'};
+		
+		String s1=new String(ch);
+		System.out.println(s1);
+		
+		System.out.println(s1.length());
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter your username: ");
+		String uname=sc.nextLine();
+		
+		System.out.println("Enter password: ");
+		String pass=sc.nextLine();
+		
+		if(uname.equals(pass)) {
+			System.out.println("Authorized");
+		}
+		else {
+			System.out.println("Wrong credentials");
+		}
+	
 	}
-
 }
-
